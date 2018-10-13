14567227d5d2ef6d16e2bd2e027436195dfbb097
Adam Doak <aydoak@gmail.com>
2018-10-13 17:55:34 -0400

    Front end skeleton build

--- client/src/components/DirtyBeaches.js
+++ client/src/components/DirtyBeaches.js
@@ -2,18 +2,22 @@
 import React, {Component} from 'react';
 import Beach from './Beach'
 
-class DirtyBeaches extends React{
+class DirtyBeaches extends Component{
   constructor(){
-    super();
+    super()
     this.state={ show: false};
-
+    this.outputBeach = this.outputBeach.bind(this)
+  }
+  outputBeach = () => {
+    const { show } = this.state
+    this.setState({ show: !show})
   }
   render(){
     return(
       <div>
-        //list
         <ul>
-          <li>Dirty Beach</li>
+          <li onClick={this.outputBeach}>Dirty Beach</li>
+            {this.state.show && <Beach/>}
         </ul>
       </div>
     )
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,14 +1,20 @@
 import React, {Component} from 'react';
+import DirtyBeaches from './DirtyBeaches';
 
-class Home extends React{
+class Home extends Component{
   render(){
     return(
       <div>
         <h1> Welcome to Dirty Beaches </h1>
-        <h3> Explanation </h3>
-        <form value='Choose search radius'>
-        <button value=' Find a dirty beach near you! '>
-
+        <h3> Find a dirty beach near you! </h3>
+        <form>
+          <label>
+           Choose search radius
+           <input type="text" />
+          </label>
+          <input type="submit" value="Find a dirty beach near you!" />
+        </form>
+        <DirtyBeaches/>
       </div>
     )
   }
