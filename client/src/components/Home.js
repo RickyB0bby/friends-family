182fea1083b6edd97e03a200e40b292685aa705d
Melidios <melidiosone@gmail.com>
2018-10-13 21:22:30 -0400

    Improved list detail functionality

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -2,19 +2,30 @@ import React, {Component} from 'react';
 import DirtyBeaches from './DirtyBeaches';
 
 class Home extends Component{
+  constructor(){
+    super();
+    this.state = { show: false };
+    this.outputBeaches = this.outputBeaches.bind(this)
+  }
+  outputBeaches = (event) => {
+    event.preventDefault()
+    const {show} = this.state
+    this.setState({show: !show})
+  }
   render(){
     return(
       <div>
         <h1> Welcome to Dirty Beaches </h1>
-        <h3> Find a dirty beach near you! </h3>
+        <h3> Description</h3>
+        <p>Let's find a dirty beach!</p>
         <form>
           <label>
-           Choose search radius
+           Enter Zip Code
            <input type="text" />
           </label>
-          <input type="submit" value="Find a dirty beach near you!" />
+          <input type="submit" value="Find a dirty beach near you!" onClick = {this.outputBeaches}/>
         </form>
-        <DirtyBeaches/>
+         {this.state.show && <DirtyBeaches/>}
       </div>
     )
   }
