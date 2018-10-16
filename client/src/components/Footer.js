767398a851b0cd7cf8d70baa38b5559f3af0c42e
William Kontos <williamjkontos@gmail.com>
2018-10-16 01:35:12 -0400

    added style, semi functioning map, and more general functionality to app

---
+++ client/src/components/Footer.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import '../BeachList.css'
+
+export default class HollywoodBeach extends Component {
+    render() {
+        return (
+        <div id="main-container">
+            <footer className="footer-container">
+              <span><i className="fab fa-facebook-square"></i></span>  
+              <span><i className="fab fa-github-square"></i></span>  
+              <span><i className="fab fa-twitter-square"></i></span>
+              <span><i className="fab fa-instagram"></i></span>
+              <p>&copy; The Dream Team 2018</p>
+            </footer>
+        </div>
+        )
+    }
+}
--- client/src/components/HollywoodBeach.js
+++ client/src/components/HollywoodBeach.js
@@ -1,10 +1,11 @@
 import React, {Component} from 'react';
+import Map from './Map'
 
 export default class HollywoodBeach extends Component {
     render() {
         return (
         <div>
-            <h1>Hollywood Beach</h1>
+            <Map />
         </div>
         )
     }
